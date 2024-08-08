3ffc324a3c5ab50b48594839229ab68858858d74
jagadeshwarant <jagadeshtj61@gmail.com>
2024-08-08 19:29:24 +0530

    updated changes

--- src/random.js
+++ src/random.js
@@ -8,12 +8,17 @@ function Randomnumber()
     {
         Setnum(Math.floor(Math.random() *100))
     }
+    function Reset()
+    {
+        Setnum(0)
+    }
 
     return(
         <div className="number">
             <p>Get random number between 0 and 100</p>
             <h1>{num}</h1>
-            <button onClick={Rannumber}>Generate</button>
+            <button onClick={Rannumber} className="one">Generate</button>
+            <button onClick={Reset}className="two">Reset</button>
         </div>
     )
 }
